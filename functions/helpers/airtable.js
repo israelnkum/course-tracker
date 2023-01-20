6b78c4ff91abb330e3f717235b5f3a0454fbef83
Amos Appiah Nkum <israelnkum@gmail.com>
2023-01-20 23:17:35 +0000

    Added airtable keys and completed local setup

--- functions/helpers/airtable.js
+++ functions/helpers/airtable.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-var Airtable = require('airtable');
-var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
 const table = base(process.env.AIRTABLE_TABLE_NAME);
 
 module.exports = { table };
